6191e08d0b3f5c0eaf4b920690934a0d5d2bc54f
Vladimir Leonov <Lion-king1986@mail.ru>
2022-10-23 20:07:54 +0300

    домашнее задание к семинару 18 10

---
+++ Homework/18 10/Task 41/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+Console.Clear();
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.WriteLine("Ведите число: ");
+        res[i] = int.Parse(Console.ReadLine());
+    }
+    
+    return res;
+}
+
+int PosNums(int[] numsarr)
+{
+    int count = 0;
+    
+    for (int i = 0; i < numsarr.Length; i++)
+    {
+        if (numsarr[i] > 0 )
+            count +=1;
+        
+    }
+    return count; 
+}
+
+void PrintArray(int[] arr)
+{
+    Console.WriteLine("Введенные числа: ");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+Console.WriteLine("Укажите колличество вводимых ниже чисел: ");
+int m = int.Parse(Console.ReadLine());
+int[] array = GetArray(m, -100000, 100000);
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine($"Колличество положительных чисел, введенных пользователем: {PosNums(array)} ");
---
+++ Homework/18 10/Task 43/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+Console.Clear();
+Console.WriteLine("Введите параметр k1 прямой y = k1*x + b1: ");
+double c1 = GetLine();
+Console.WriteLine("Введите параметр b1 прямой y = k1*x + b1: ");
+double d1 = GetLine();
+Console.WriteLine("Введите параметр k2 прямой y = k2*x + b2: ");
+double c2 = GetLine();
+Console.WriteLine("Введите параметр b2 прямой y = k2*x + b2: ");
+double d2 = GetLine();
+if (c1 == c2)
+        Console.WriteLine("Прямые не пересекаются ");
+else
+    PrintPoint();
+
+
+double GetLine()
+{
+    double k1 = Convert.ToDouble(Console.ReadLine());
+    return k1;
+}
+
+(double,double) GetPoint(double m3, double n3, double m4, double n4)
+{
+    double y = 0;
+    double x1 = 0;
+    x1 += (n4 - n3) / (m3 - m4);
+    y += (m3 * x1) + n3;
+    var result= (x1,y);
+    return result;
+}
+
+void PrintPoint()
+{
+    Console.Write($"Координаты точки пересечения заданных прямых {GetPoint(c1, d1, c2, d2)}  ");
+}
